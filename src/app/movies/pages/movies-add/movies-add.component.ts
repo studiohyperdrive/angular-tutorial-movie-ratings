764034cfca672d6eb8cab6d07eafa65194357f72
Benjamin Naesen <benjamin@naesen.be>
2021-03-08 14:07:30 +0100

    feat(app): generate modules, components, services

---
+++ src/app/movies/pages/movies-add/movies-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-add',
+  templateUrl: './movies-add.component.html',
+  styleUrls: ['./movies-add.component.scss']
+})
+export class MoviesAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
